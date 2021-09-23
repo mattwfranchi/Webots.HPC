500a90b99bc9fe88a02ed67b2b36352407e17f23
mattwfranchi <mattwfranc@gmail.com>
2021-09-23 16:46:07 -0400

    reset script functionality

--- Scripts/webotshpc_resetSUMOport.sh
+++ Scripts/webotshpc_resetSUMOport.sh
@@ -1,1 +1,1 @@
-awk '$0 ~ /port/ { $1 = "  " $1; $2++ } 1' ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt > tmp$$ && mv tmp$$ ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt
+awk '$0 ~ /port/ { $1 = "  " $1; $2 = 8873 } 1' ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt > tmp$$ && mv tmp$$ ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt
