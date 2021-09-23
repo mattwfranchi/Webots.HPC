f53c8ad3fec86990460b794d40995a2b387c1546
mattwfranchi <mattwfranc@gmail.com>
2021-09-23 16:44:54 -0400

    added script to reset SUMO interface port

---
+++ Scripts/webotshpc_resetSUMOport.sh
@@ -0,0 +1,1 @@
+awk '$0 ~ /port/ { $1 = "  " $1; $2++ } 1' ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt > tmp$$ && mv tmp$$ ~/Webots.HPC/Simulations/HighwayMerge/worlds/LinkonMKZmyMap.wbt
