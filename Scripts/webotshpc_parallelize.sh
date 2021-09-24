87f162e91b97a3acf4e193be3adde0d8aa68166a
mattwfranchi <mattwfranc@gmail.com>
2021-09-24 10:50:42 -0400

    export functionality working

--- Scripts/webotshpc_parallelize.sh
+++ Scripts/webotshpc_parallelize.sh
@@ -64,7 +64,7 @@ while getopts "hj:n:q:s:c:m:i:w:e" o; do
     w) # Enter walltime
       Walltime=${OPTARG};;
     e) # Export Flag
-      EXPORT=true
+      EXPORT=true;;
     \?) # Invalid option
     echo "Error: Invalid option"
     Help
@@ -78,29 +78,29 @@ shift $((OPTIND-1))
 echo "Webots.HPC: Parallelized Job Submission Script"
 echo
 echo "-------------- Summary of Resources Requested --------------"
-echo "Number of Instances to Trigger: 		  $Num_instances"
-echo "Number of Nodes to Request (Select): 	$Select"
-echo "Number of CPUs to Request:		        $Ncpus"
-echo "Amount of Memory to Request:		      $Memory"
-echo "Type of Interconnect:			            $Interconnect"
-echo "Walltime Requested:		         	      $Walltime"
-echo "Job Queue (If Specified):			        $Queue"
+echo "Number of Instances to Trigger:		$Num_instances"
+echo "Number of Nodes to Request (Select):	$Select"
+echo "Number of CPUs to Request:		$Ncpus"
+echo "Amount of Memory to Request:		$Memory"
+echo "Type of Interconnect:			$Interconnect"
+echo "Walltime Requested:			$Walltime"
+echo "Job Queue (If Specified):		$Queue"
 echo "------------------------------------------------------------"
 echo
 
 # Export Job
 if [ $EXPORT = true ]
 then
-  echo "#!/bin/bash" > 'parallelized_${Job_name}'
-  echo "#PBS -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime" >> 'parallelized_${Job_name}'
-  echo "#PBS -J 1-$Num_instances" >> 'parallelized_${Job_name}'
+  echo "#!/bin/bash" > parallelized_$Job_name
+  echo "#PBS -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime" >> parallelized_$Job_name
+  echo "#PBS -J 1-$Num_instances" >> parallelized_$Job_name
   if [ ! -z "$Queue" ]
   then
-    echo "#PBS -q $Queue" >> 'parallelized_${Job_name}'
+    echo "#PBS -q $Queue" >> parallelized_$Job_name
   fi
   echo
   echo
-  cat $Job_name >> 'parallelized_${Job_name}'
+  cat $Job_name >> parallelized_$Job_name
 else
   # Triggering Customized Job
   if [ -z "$Queue" ]
