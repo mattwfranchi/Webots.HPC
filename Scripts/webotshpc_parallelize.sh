c97eec66cc7a817861c5b902ce128a385dc4ff8b
mattwfranchi <mattwfranc@gmail.com>
2021-09-24 10:21:46 -0400

    draft of job export functionality

--- Scripts/webotshpc_parallelize.sh
+++ Scripts/webotshpc_parallelize.sh
@@ -39,9 +39,10 @@ Ncpus=5
 Memory="93gb"
 Interconnect="hdr"
 Walltime="00:45:00"
+EXPORT=false
 
 # Processing Command Line Arguments
-while getopts "hj:n:q:s:c:m:i:w" o; do
+while getopts "hj:n:q:s:c:m:i:w:e" o; do
   case "${o}" in
     h) # display help
     Help
@@ -62,6 +63,8 @@ while getopts "hj:n:q:s:c:m:i:w" o; do
       Interconnect=${OPTARG};;
     w) # Enter walltime
       Walltime=${OPTARG};;
+    e) # Export Flag
+      EXPORT=true
     \?) # Invalid option
     echo "Error: Invalid option"
     Help
@@ -75,24 +78,37 @@ shift $((OPTIND-1))
 echo "Webots.HPC: Parallelized Job Submission Script"
 echo
 echo "-------------- Summary of Resources Requested --------------"
-echo "Number of Instances to Trigger: 		$Num_instances"
+echo "Number of Instances to Trigger: 		  $Num_instances"
 echo "Number of Nodes to Request (Select): 	$Select"
-echo "Number of CPUs to Request:		$Ncpus"
-echo "Amount of Memory to Request:		$Memory"
-echo "Type of Interconnect:			$Interconnect"
-echo "Walltime Requested:			$Walltime"
-echo "Job Queue (If Specified):			$Queue"
+echo "Number of CPUs to Request:		        $Ncpus"
+echo "Amount of Memory to Request:		      $Memory"
+echo "Type of Interconnect:			            $Interconnect"
+echo "Walltime Requested:		         	      $Walltime"
+echo "Job Queue (If Specified):			        $Queue"
 echo "------------------------------------------------------------"
 echo
 
-# Triggering Customized Job
-
-if [ -z "$Queue" ]
+# Export Job
+if [ $EXPORT = true ]
 then
-	echo
-	echo "No queue specified, so using default." 	
-	qsub -J 1-$Num_instances -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime $Job_name
+  echo "#!/bin/bash" > 'parallelized_${Job_name}'
+  echo "#PBS -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime" >> 'parallelized_${Job_name}'
+  echo "#PBS -J 1-$Num_instances" >> 'parallelized_${Job_name}'
+  if [ ! -z "$Queue" ]
+  then
+    echo "#PBS -q $Queue" >> 'parallelized_${Job_name}'
+  fi
+  echo
+  echo
+  cat $Job_name >> 'parallelized_${Job_name}'
 else
-	qsub -q $Queue -J 1-$Num_instances -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime $Job_name
+  # Triggering Customized Job
+  if [ -z "$Queue" ]
+  then
+  	echo
+  	echo "No queue specified, so using default."
+  	qsub -J 1-$Num_instances -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime $Job_name
+  else
+  	qsub -q $Queue -J 1-$Num_instances -l select=$Select:ncpus=$Ncpus:mem=$Memory:interconnect=$Interconnect,walltime=$Walltime $Job_name
+  fi
 fi
-
