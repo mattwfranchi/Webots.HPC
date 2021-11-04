8237695d09987b32a18f3a46d2f412888a95e7e3
mattwfranchi <mattwfranc@gmail.com>
2021-11-04 12:25:46 -0400

    added agent code

--- Simulations/cartpole_dqrl_tensorflow/controllers/DeepQAgent.py
+++ Simulations/cartpole_dqrl_tensorflow/controllers/DeepQAgent.py
@@ -1,1 +1,129 @@
+# Matt Franchi
+# Webots.HPC - CartPole Q Learning
+# November 2021
+
+# IMPORTS
 from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+import reverb
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+
+# VIRTUAL DISPLAY
+# Set up a virtual display for rendering OpenAI gym environments.
+display = pyvirtualdisplay.Display(visible=0,size=(1400,900)).start()
+
+tf.version.VERSION
+
+# HYPERPARAMETERS
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps =100 # @param {type:"integer"}
+collect_steps_per_iteration = 1 # @param {type:"integer"}
+replay_buffer_max_length = 100000 # @param {type:"integer"}
+
+batch_size = 64 # @param {type:"integer"}
+learning_rate = 1e-3 # @param {type:"number"}
+log_interval = 200 # @param {type:"integer"}
+
+num_eval_episodes = 10 # @param {type:"integer"}
+eval_interval = 1000 # @param {type:"integer"}
+
+# ENVIRONMENT
+env_name = 'CartPole-v0'
+env = suite_gym.load(env_name)
+
+#@test {"skip": true}
+env.reset()
+PIL.Image.fromarray(env.render())
+
+print('Observation Spec:')
+print(env.time_step_spec().observation)
+print('Reward Spec:')
+print(env.time_step_spec().reward)
+print('Action Spec:')
+print(env.action_spec())
+
+
+time_step = env.reset()
+print('Time step:')
+print(time_step)
+
+action = np.array(1, dtype=np.int32)
+
+next_time_step = env.step(action)
+print('Next time step:')
+print(next_time_step)
+
+train_py_env = suite_gym.load(env_name)
+eval_py_env = suite_gym.load(env_name)
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+# Closing environments: prevents no attribute 'dumps' error
+train_env.close()
+eval_env.close()
+
+# AGENT
+fc_layer_params = (100,50)
+action_tensor_spec = tensor_spec.from_spec(env.action_spec())
+num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+# Define a helper function to create Dense layers configured with the right activation and kernel initializer
+def dense_layer(num_units):
+    return tf.keras.layers.Dense(
+        num_units,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+# QNetwork consists of a sequence of Dense layers followed by a dense layer with
+# 'num_actions' units to generate one q_value per available action as its output
+dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+q_values_layer = tf.keras.layers.Dense(
+    num_actions,
+    activation=None,
+    kernel_initializer=tf.keras.initializers.RandomUniform(
+        minval=-0.03,maxval=0.03),
+    bias_initializer=tf.keras.initializers.Constant(-0.2))
+q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+# POLICIES
