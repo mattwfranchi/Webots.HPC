613bacdaa1dcc113de1a343397a09d53bc7ada02
mattwfranchi <mattwfranc@gmail.com>
2021-11-18 12:49:27 -0500

    workflow.py -> transformation catalog

--- Scripts/workflow.py
+++ Scripts/workflow.py
@@ -17,9 +17,50 @@ props.write()
 simulationSpec = File("../cav_highwaymerging.pbs")
 rc = ReplicaCatalog()\
     .add_replica("local",simulationSpec,Path(".").resolve() \ "cav_highwaymerging.pbs")
-    
+
 
 # TRANSFORMATION CATALOG (EXECUTABLES)
+
+## Batch of Simulations [JOB]
+simulation = Transformation(
+    "simulation",
+    site="condorpool",
+    pfn="/usr/bin/pegasus-keg",
+    is_stageable=False,
+    arch=Arch.X86_64,
+    os_type=OS.LINUX
+)
+
+## Merge Output [JOB]
+merge = Transformation(
+    "merge",
+    site="condorpool",
+    pfn="/usr/bin/pegasus-keg",
+    is_stageable=False,
+    arch=Arch.X86_64,
+    os_type=OS.LINUX
+)
+
+## ML Preprocessing [JOB]
+ml_preprocessing = Transformation(
+    "ml_preprocesssing",
+    site="condorpool",
+    pfn="/usr/bin/pegasus-keg",
+    is_stageable=False,
+    arch=Arch.X86_64,
+    os_type=OS.LINUX
+)
+
+## ML Processing [JOB]
+ml_processing = Transformation(
+    "ml_processing",
+    site="condorpool",
+    pfn="/usr/bin/pegasus-keg",
+    is_stageable=False,
+    arch=Arch.X86_64,
+    os_type=OS.LINUX
+)
+
 
 
 # WORKFLOW SPECIFICATION
