04ef0275d1d496520a28949c3ad5b064ce6db3d3
mattwfranchi <mattwfranc@gmail.com>
2021-11-18 13:23:32 -0500

    draft of workflow.py finished

--- Scripts/workflow.py
+++ Scripts/workflow.py
@@ -21,46 +21,74 @@ rc = ReplicaCatalog()\
 
 # TRANSFORMATION CATALOG (EXECUTABLES)
 
-## Batch of Simulations [JOB]
-simulation = Transformation(
-    "simulation",
-    site="condorpool",
-    pfn="/usr/bin/pegasus-keg",
-    is_stageable=False,
-    arch=Arch.X86_64,
-    os_type=OS.LINUX
-)
+## sbatch
+sbatch = Transformation(
+                "sbatch",
+                site="condorpool",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=False,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX
+            )
 
-## Merge Output [JOB]
-merge = Transformation(
-    "merge",
-    site="condorpool",
-    pfn="/usr/bin/pegasus-keg",
-    is_stageable=False,
-    arch=Arch.X86_64,
-    os_type=OS.LINUX
-)
+## qsub
+qsub = Transformation(
+                "qsub",
+                site="condorpool",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=False,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX
+            )
 
-## ML Preprocessing [JOB]
-ml_preprocessing = Transformation(
-    "ml_preprocesssing",
-    site="condorpool",
-    pfn="/usr/bin/pegasus-keg",
-    is_stageable=False,
-    arch=Arch.X86_64,
-    os_type=OS.LINUX
-)
+## sh
+sh = Transformation(
+                "sh",
+                site="condorpool",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=False,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX
+            )
 
-## ML Processing [JOB]
-ml_processing = Transformation(
-    "ml_processing",
-    site="condorpool",
-    pfn="/usr/bin/pegasus-keg",
-    is_stageable=False,
-    arch=Arch.X86_64,
-    os_type=OS.LINUX
-)
-
-
+## Rscript
+Rscript = Transformation(
+                "Rscript",
+                site="condorpool",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=False,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX
+            )
+## Creating Transformation Catalog
+tc = TransformationCatalog()\
+    .add_transformations(sbatch,qsub,sh,Rscript)
+    .write()
 
 # WORKFLOW SPECIFICATION
+wf = Workflow("webots.hpc")
+
+simulationOutput = File("output.csv")
+job_simulation = Job(sbatch)\
+    .add_args("-a", "sbatch", "-T", "3", "-i", simulationSpec, "-o", simulationOutput)\
+    .add_inputs(simulationSpec)\
+    .add_outputs(simulationOutput)
+
+wf.add_jobs(job_simulation)
+
+try:
+    wf.write()
+    wf.graph(include_files=True,label="xform-id",output="graph.png")
+except PegasusClientError as e:
+    print(e)
+
+try:
+    wf.plan(submit=True)\
+        .wait()
+except PegasusClientError as e:
+    print(e)
+
+try:
+    wf.statistics()
+except PegasusClientError as e:
+    print(e)
